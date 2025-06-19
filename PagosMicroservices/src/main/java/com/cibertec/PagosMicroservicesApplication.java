969de2ffdb7930140e9a13669191246576fdc482
juanespinoza94 <jcetal1994@gmail.com>
2025-06-18 21:57:37 -0500

    Estructura inicial de microservicios

---
+++ PagosMicroservices/src/main/java/com/cibertec/PagosMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagosMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagosMicroservicesApplication.class, args);
+	}
+
+}
